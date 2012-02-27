58c9c8d78025854de779dc1908075f305bdab16f
Cargo Kult <cargokultist@gmail.com>
2012-02-27 16:41:10 +0200

    added user repo activity parser and a test file

--- README.md
+++ README.md
@@ -11,12 +11,18 @@ A Don't Break the Chain Calendar. It feeds upon user's activity on github.
 * Days with github activity should be marked.
 
 ## Tasks
-* create a hosted site for this
+* create a hosted site for this OK
 * identify current github user
 * clonned repo should identify the new user
-* query user github activity
+* query user github activity OK
 * map user activity on calendar days
 * prettify the calendar view
 
+## Open Issues
+* Is there a github api v3 javascript impl?
+* Which is the easiest way to publish on gh-pages? Please see scala_school and JavaScript-Garden.
+
 ## References
 * http://www.persistenceunlimited.com/2007/09/seinfeld-says-dont-break-the-chain/
+* http://twitter.github.com/scala_school/
+* https://github.com/BonsaiDen/JavaScript-Garden
--- site/javascript/nasreddin.js
+++ site/javascript/nasreddin.js
@@ -1,13 +1,11 @@
-gh.authenticate("fitzgen", "sdfk32we-FAKE-uydfs7f-rhrwe8r7");
-var huddlej = gh.user("huddlej");
-huddlej.show(function (data) {
+var username = "cargokult";
+var user = gh.user(username);
+user.show(function (data) {
     console.log(data.user);
 });
-huddlej.repos(function (data) {
-    console.log("Number of repos: " + data.repositories.length);
+user.allRepos(function (data) {
+    data.repositories.forEach(function (repo) {
+        console.log("Repo " + repo.name + " created at " + repo.created_at + " last pushed at " + repo.pushed_at);
+    });
 });
-var wujs = gh.repo("fitzgen", "wu.js")
-wujs.show(function (data) {
-    console.log("Number of watchers: " + data.repository.watchers);
-});
-wujs.update({ has_wiki: 0 }); // Unfortunately, no callbacks with POSTs :(
+
