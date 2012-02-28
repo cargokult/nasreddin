7496d25f1469962cf8131c1339a0ed303ba81c4b
Cargo Kult <cargokultist@gmail.com>
2012-02-28 15:19:05 +0200

    cleanup

--- site/javascript/nasreddin.js
+++ site/javascript/nasreddin.js
@@ -1,26 +1,17 @@
 $(document).ready(function() {
-	var date = new Date();
-	var d = date.getDate();
-	var m = date.getMonth();
-	var y = date.getFullYear();
-
 	var username = "cargokult";
 	var user = gh.user(username);
-	user.show(function (data) {
-    	console.log(data.user);
-	});
 	user.allRepos(function (data) {
 		var events = [];
 		var index = 0;
-    	data.repositories.forEach(function (repo) {
-       	console.log("Repo " + repo.name + " created at " + repo.created_at + " last pushed at " + repo.pushed_at);
-    		events[index] = {
+		data.repositories.forEach(function (repo) {
+		events[index] = {
 				title: repo.name,
 				start: repo.created_at
-			};
-			index++;
-    	});
-    	$('#calendar').fullCalendar({
+				};
+		index++;
+		});
+		$('#calendar').fullCalendar({
 				header: {
 				left: 'prev,next today',
 				center: 'title',
